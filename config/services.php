db2ae1b52e2111ce247e95854099d941e35bf2cb
Niels Verbaken <nielsverbaken@outlook.com>
2019-02-23 15:01:50 +0100

    chg Github OAuth callback url config

--- config/services.php
+++ config/services.php
@@ -43,7 +43,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'auth/github',
+        'redirect' => 'login/github/callback',
     ]
 
 ];
